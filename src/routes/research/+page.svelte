71795db1c4c95e41e6e3026bd48d5d3e78fdd876
BectorWhattt <wutthebector@gmail.com>
2026-03-15 23:50:45 +0800

    features: research

---
+++ src/routes/research/+page.svelte
@@ -0,0 +1,10 @@
+
+
+<div class="absolute m-0 p-0 top-0 w-full h-[225px] bg-cover bg-center">
+  <div class="absolute inset-0 bg-gradient-to-r from-[rgba(0,0,0,0.2)] via-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.2)]"></div>
+  <h1 class="relative text-white text-[120px] pl-[65px] pt-[50px] font-bold">Research</h1>
+</div>
+
+<div class="absolute h-[500px] w-full flex items-center justify-center bg-gradient-to-b from-gray-200 to-gray-400 p-4">
+  haha
+</div>
