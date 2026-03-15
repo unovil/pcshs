15b860f2bf7505fd99c7b98a32530819c0f1c9e0
BectorWhattt <wutthebector@gmail.com>
2026-03-16 00:52:50 +0800

    head

--- src/routes/research/+page.svelte
+++ src/routes/research/+page.svelte
@@ -1,10 +1,24 @@
+<script lang="ts">
+  import heroBG from "./bg-research.jpg";
+  import PageHeader from '$lib/components/page-header.svelte';
+
+</script>
 
 
-<div class="absolute m-0 p-0 top-0 w-full h-[225px] bg-cover bg-center">
-  <div class="absolute inset-0 bg-gradient-to-r from-[rgba(0,0,0,0.2)] via-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.2)]"></div>
-  <h1 class="relative text-white text-[120px] pl-[65px] pt-[50px] font-bold">Research</h1>
+<svelte:head>
+  <title>Research</title>
+</svelte:head>
+
+<div>
+  <PageHeader title="Campus Map" img={heroBG} />
+  <div class="container">
+    <button>Button 1</button>
+    <button>Button 2</button>
+    <button>Button 3</button>
+    <button>Button 4</button>
+  </div>
 </div>
 
-<div class="absolute h-[500px] w-full flex items-center justify-center bg-gradient-to-b from-gray-200 to-gray-400 p-4">
-  haha
-</div>
+<style>
+  @import "./styles.css";
+</style>
