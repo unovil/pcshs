4653586e1286450557dc2e682cd0518b2dbe6eee
Juan Miguel Villegas <70586029+unovil@users.noreply.github.com>
2026-03-15 12:10:46 +0800

    added about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let i = $state(1);
+
+  function add1() {
+    if (i >= 7) i = 7;
+    else i++;
+  }
+
+  function subtract1() {
+    if (i <= 1) i = 1;
+    else i--;
+  }
+</script>
+
+<div>
+  <p class="font-bold text-4xl font-serif text-blue-700">About Us</p>
+  <p class="text-lg">{i}</p>
+  <button onclick={add1}>Add 1</button>
+  <button onclick={subtract1}>Subtract 1</button>
+</div>
